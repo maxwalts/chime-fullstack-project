3894af1d54123c690576400435cd8e4380b633f2
mwalts <mwalts@umich.edu>
2020-08-09 14:50:28 -0500

    menu and tag style

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Segment, Header, Label } from "semantic-ui-react";
 
 const Menu = ({ items }) => {
   return (
-    <div>
+    <Segment.Group stacked>
+      <Header
+        style={{
+          backgroundColor: "#179a5c",
+          color: "#F5F5F5",
+          textAlign: "center",
+        }}
+        as="h2"
+        attached="top"
+      >
+        Your Menu
+      </Header>
       {items.map((item) => {
         return (
-          <p>
-            {item.name}
-            {item.tag}
-          </p>
+          <Segment padded="very" style={{ backgroundColor: "#1ec677" }}>
+            <Header style={{ color: "#F5F5F5" }} as="h2">
+              {item.name}
+              <Label
+                style={
+                  item.tag
+                    ? { color: "#c61e6d" }
+                    : { backgroundColor: "#1ec677" }
+                }
+              >
+                {item.tag}
+              </Label>
+            </Header>
+          </Segment>
         );
       })}
-    </div>
+    </Segment.Group>
   );
 };
 
