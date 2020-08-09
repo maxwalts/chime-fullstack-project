88eba99abbbe640bf3b7f66ec55626338d7419ca
mwalts <mwalts@umich.edu>
2020-08-08 23:11:57 -0500

    update endpoints; get/post functional with or without tag

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -13,14 +13,29 @@ db.init_app(app)
 
 @app.route("/get/menuitems", methods=["GET"])
 def get_menuitems():
-    result = MenuItem.query.all()
-    
-    return {
-        "menu items": [(dict(row.items())) for row in result]
-    }, 200
+    query = MenuItem.query.all()
+    items = []
+
+    for each in query:
+        items.append({'name' : each.name, 'tag' : each.tag})
+
+    return jsonify({'menu items' : items}), 200
 
 @app.route("/post/menuitems", methods=["POST"])
 def post_menuitems():
+    request_data = request.get_json()
+
+    name = request_data["name"]
+
+    # I won't always be adding a tag to an item
+    if 'tag' in request_data:
+        tag = request_data["tag"]
+        new_menuitem = MenuItem(name=name, tag=tag)
+    else:
+        new_menuitem = MenuItem(name=name)
+
+    db.session.add(new_menuitem)
+    db.session.commit()
 
     return "created", 201
 
--- backend/models/MenuItem.py
+++ backend/models/MenuItem.py
@@ -8,4 +8,4 @@ class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
     # I think I will eventually add the tag to a menuitem object so it would be searchable
-    # tag = db.Column(db.String(10), nullable=True)
+    tag = db.Column(db.String(10), nullable=True)
